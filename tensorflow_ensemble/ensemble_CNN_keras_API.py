3623d8b05810f275dda846dc365776a9ac903530
zhongling <jzljohn18@berkeley.edu>
2018-01-16 20:47:01 -0800

    implemnet ensembele predict using averaging

--- tensorflow_ensemble/ensemble_CNN_keras_API.py
+++ tensorflow_ensemble/ensemble_CNN_keras_API.py
@@ -1,13 +1,26 @@
+'''
+The model created ensemble of CNNs through averaging prediction results.
+Author: Zhongling
+'''
+
 import numpy as np
 import time
+import os
 from Utils import load_pd_data
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import InputLayer, Input
 from tensorflow.python.keras.layers import Reshape, MaxPooling2D
 from tensorflow.python.keras.layers import Conv2D, Dense, Flatten
-
+from keras.models import load_model
 from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.metrics import categorical_accuracy
 from Utils import next_batch
+
+'''
+The class create CNN model objects. Include build, compile, train, save and evaluate.
+Users could also define other model objects and modify ensemble function to train and
+save other models.
+'''
 class CNN_model(object):
 
     def __init__(self, image_size, num_channels, num_classes):
@@ -61,13 +74,16 @@ class CNN_model(object):
                           loss= loss_metric,
                           metrics=metrics)
 
-    def train(self, model_name = None, epochs = 10, batch_size = 128, save_option = False):
+    def train(self, model_name = None, epochs = 10, batch_size = 128):
         if self.model is not None:
             self.model.fit(x=self.X, y=self.Y, epochs= epochs, batch_size= batch_size)
-            if save_option is True:
-                path = './models/'
-                self.model.save(path + 'model_{}.h5py'.format(model_name))
 
+    def save(self, filepath):
+        if self.model is not None:
+            self.model.save(filepath + ".h5py")
+            model_json = self.model.to_json()
+            with open(filepath + ".json", "w") as json_file:
+                json_file.write(model_json)
     def evaluate_train(self):
         if self.model is not None:
             result = self.model.evaluate(self.X, self.Y)
@@ -87,34 +103,62 @@ class CNN_model(object):
         cls_pred = np.argmax(y_pred, axis=1)
         return y_pred, cls_pred
 
-
-def CNN_ensemble(num_networks, train_X, train_Y, batch_size = 128):
-    image_size = 96
+'''
+The function trains and saves n networks, currently all CNNs. This function could be extended
+into other models as well.
+'''
+def CNN_ensemble(image_size, num_networks, num_classes, train_X, train_Y,  epochs = 5, batch_size = 128):
     num_channels= 1
-    num_classes = 9
     list_of_model = []
     for i in range(num_networks):
-        # for j in range(epochs..)
-        X_batch, Y_batch = next_batch(train_X, train_Y, batch_size)
+       # for j in range(epochs):
+        #X_batch, Y_batch = next_batch(train_X, train_Y, batch_size)
         model = CNN_model(image_size, num_channels, num_classes)
-        model.load_train_data(X_batch, Y_batch)
+        model.load_train_data(train_X, train_Y)  #######################
         model.build_model()
         optimizer = Adam(lr=1e-3)
         model.compile_model(optimizer)
-        model.train(epochs=5)
-        model.evaluate_train()
+        model.train(epochs= epochs, batch_size=batch_size)
+        #model.evaluate_train()
+        model_name = "CNN_model_{0}".format(i)
+        model.save("./model/" + model_name)    #####save the model here
         if model is None:
-            print ('missing model')
-        else:
-            y_pred, cls_pred = model.predict(train_X)
-            print(cls_pred)
+            print ("missing model_{0}".format(i))
 
+'''
+The function loads n models and make predictiona using average of n networks.
+'''
+def ensemble_predict(number_of_models, num_classes, test_set, filepath= '../model/', pattern= ".h5py"):
+    all_files = os.listdir(filepath)
+    if len(all_files) == 0:
+        print('No Models in the directory. Please first train and save models.')
+    else:
+        predictions = np.zeros((test_set.shape[0], num_classes))
+        for (i,filename) in enumerate(all_files):
+            count = 0
+            if pattern in filename:
+                model = load_model(filepath + filename)
+                preds = model.predict(test_set)
+                predictions += preds
+                #cls_preds = np.argmax(preds, axis=1)
+        predictions = predictions / num_networks
+        return predictions
 
 
 if __name__ == '__main__':
     print('Load in Data ...')
-    train_X, train_Y, _, _ = load_pd_data('../data/pixel_kaggle.pd')
+    train_X, train_Y, _, _ = load_pd_data('../data/pixel_nocomplex_train.pd')
+    test_X, test_Y, _, _ = load_pd_data('../data/pixel_nocomplex_test.pd')
     epochs = 10
     batch_size = 128
-    num_networs = 3
-    CNN_ensemble(3, train_X, train_Y, batch_size)
+    num_networks = 3
+    num_classes = 8
+    image_size = 96
+    CNN_ensemble(image_size, num_networks, num_classes, train_X, train_Y, epochs, batch_size)
+    preds = ensemble_predict(num_networks, num_classes, test_X)
+    preds_cls = np.argmax(preds, axis=1)
+    print("The prediction for first 100 images are: ")
+    print(preds[0:99])
+    # measure accuracy
+    acc = np.sum(preds_cls == test_Y) / len(preds_cls)
+    print("The accuracy of ensemble model is: {0}".format(acc))
